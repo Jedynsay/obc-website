50a3ab2a80da7f753724ea91b78e65078215f7d5
Jedynsay <49426053+Jedynsay@users.noreply.github.com>
2025-08-15 18:50:01 +0800

    Updated useTournamentRegistrationData.ts

--- src/components/Views/hooks/useTournamentRegistrationData.ts
+++ src/components/Views/hooks/useTournamentRegistrationData.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 import { Tournament } from '../../../types';
 
 export function useTournamentRegistrationData(tournament: Tournament, onClose: () => void) {
