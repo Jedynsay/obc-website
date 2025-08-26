8c465a9e48538999e103fc057057ab5e7843d5d0
Jedynsay <49426053+Jedynsay@users.noreply.github.com>
2025-08-27 01:01:14 +0800

    Updated TournamentRegistration.tsx

--- src/components/Views/TournamentRegistration.tsx
+++ src/components/Views/TournamentRegistration.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabase";
-import { useAuth } from "../../contexts/AuthContext";
+import { useAuth } from "../contexts/AuthContext";
 import { useConfirmation } from "../../contexts/ConfirmationContext";
 import {
   Dialog,
